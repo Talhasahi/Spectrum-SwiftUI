d4c2982227102e4c679cb20da0c44a80255e8258
TalhaAzam <talhasahi86@gmail.com>
2020-05-18 14:15:03 +0500

    Initial Commit

---
+++ Spectrum-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Spectrum-SwiftUI
+//
+//  Created by Talha on 18/05/2020.
+//  Copyright © 2020 Talha. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
